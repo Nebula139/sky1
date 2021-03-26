38828dbbdf6e7ae6d8c08b2c62c91d35d385720e
Nebula139 <78951954+Nebula139@users.noreply.github.com>
2021-03-26 10:50:39 +0300

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,4 @@
+print("4*100-54")
+Answer=input()
+print("Your answer",Answer)
+print("Right answer", 4*100-54)
---
+++ numbers.py
@@ -0,0 +1,8 @@
+Number1=float(input())
+Number2=float(input())
+Number3=float(input())
+Number4=float(input())
+S1=Number1+Number2
+S2=Number3+Number4
+D=S1/S2
+print("%.2f" %D)
